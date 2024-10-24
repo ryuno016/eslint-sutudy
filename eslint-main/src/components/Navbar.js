b996521de4f3ee7e8a4a88aa540879efd1a6ee9d
多多納 龍之介 <ryuno8901398@gmail.com>
2024-10-24 10:32:01 +0900

    navvar-fix

--- eslint-main/src/components/Navbar.js
+++ eslint-main/src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types'; // PropTypesのインポート
 
-function Navbar(props) {
-    return <nav>{props.title}</nav>;
+function Navbar({ title }) {
+  return <nav>{title}</nav>;
 }
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired, // titleは必須の文字列として定義
+};
+
+Navbar.defaultProps = {
+  title: 'Default Navbar Title', // デフォルトのタイトルを設定
+};
 
 export default Navbar;
