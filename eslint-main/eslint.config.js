9ef27bf1d5d3e113bcefb250a793376956afc407
多多納 龍之介 <ryuno8901398@gmail.com>
2024-10-28 15:46:46 +0900

    設定とテストコードの変更

--- eslint-main/eslint.config.js
+++ eslint-main/eslint.config.js
@@ -1,18 +1,21 @@
-export default [
-  {
-    files: ["src/**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module"
-    },
-    env: {
-      browser: true
-    },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    rules: {
-      "no-console": "error",
-      "eqeqeq": ["error", "always"],
-      "curly": "error"
+export default {
+  files: ["src/**/*.js"],
+  languageOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module"
+  },
+  env: {
+    browser: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  settings: {
+    react: {
+      version: "17.0"
     }
+  },
+  rules: {
+    "no-console": "error",
+    "eqeqeq": ["error", "always"],
+    "curly": "error"
   }
-];
+};
--- eslint-main/src/components/TestComponent.js
+++ eslint-main/src/components/TestComponent.js
@@ -1,3 +1,28 @@
-function handleClick(event) {
-  if (event == null) console.log('Event is null');  // 厳密等価演算子の使用とconsole.logの禁止、波括弧の強制
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Navbar(props) {
+    return <nav>{props.title}</nav>;
 }
+
+// PropTypesでpropsの型チェックを追加します
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired
+};
+
+const MainContent = () => {
+    const items = ['Item1', 'Item2', 'Item3'];
+    return (
+        <div>
+            <h1>Main Content</h1>
+            <ul>
+                {/* key属性がない状態 */}
+                {items.map(item => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export { Navbar, MainContent };
